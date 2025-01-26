de5821392c252027b29548e0a3dac6a72facbe2c
ChusDiez <diezmartinchus@gmail.com>
2025-01-26 06:08:04 +0100

    origin/main

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'https://mi-backend.up.railway.app';
 const api = axios.create({ baseURL });
 
 export default api;
